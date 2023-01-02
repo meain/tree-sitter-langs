bf741a95b172053f3f6d86e6d15854a211da32e7
Abin Simon <mail@meain.io>
2023-01-02 11:16:47 +0530

    fix windows

--- .github/script/setup-cask.ps1
+++ .github/script/setup-cask.ps1
@@ -1,2 +1,2 @@
-git clone -b github-actions https://github.com/ubolonton/cask "$env:UserProfile\.cask"
+git clone -b fix-cask-publish https://github.com/meain/cask "$env:UserProfile\.cask"
 echo "$env:UserProfile\.cask\bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
